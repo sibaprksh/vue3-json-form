727bc81efc615b2dec0ef2d249032f4e0bccab91
sibaprksh <siba4269@gmail.com>
2023-03-21 12:15:17 +0530

    4 files modified, 2 added

---
+++ utils.js
@@ -0,0 +1,13 @@
+function isDecimalNumber({ options }) {
+  const val = this.value;
+  const { max = Infinity, mantissa } = options;
+  if (val > max) return false;
+  const splt = val.split('.');
+  if (splt.length > 2) return false;
+  if (mantissa && splt[1]) {
+    const { minlength = 1, maxlength = Infinity } = mantissa;
+    if (splt[1].length > maxlength) return false;
+    if (splt[1].length < minlength) return false;
+  }
+  return true;
+}
